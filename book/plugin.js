2a0f44aea08dacdd0c9c0efbcc5ebec80af35c6b
cades kao <cadeskao@gmail.com>
2015-12-20 21:58:42 +0800

    support custom port
    this commit make livereload port configurable.

--- book/plugin.js
+++ book/plugin.js
@@ -1,11 +1,13 @@
-(function() {
-  var newEl = document.createElement('script'),
-      firstScriptTag = document.getElementsByTagName('script')[0];
+require(["gitbook"], function(gitbook) {
+  gitbook.events.bind("start", function(e, config) {
+    var newEl = document.createElement('script'),
+        firstScriptTag = document.getElementsByTagName('script')[0],
+        port = config.livereload.port || 35729;
 
-  if (firstScriptTag) {
-    newEl.async = 1;
-    newEl.src = '//' + window.location.hostname + ':35729/livereload.js';
-    firstScriptTag.parentNode.insertBefore(newEl, firstScriptTag);
-  }
-
-})();
+    if (firstScriptTag) {
+      newEl.async = 1;
+      newEl.src = '//' + window.location.hostname + ':' + port + '/livereload.js';
+      firstScriptTag.parentNode.insertBefore(newEl, firstScriptTag);
+    }
+  });
+});
